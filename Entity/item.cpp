0c3d223ab4ec2c710621ff0dae5d6ab7e3fd336a
ShiroXqC <hiamgosunabu@gmail.com>
2025-05-01 22:13:38 +0700

    Piss

--- Entity/item.cpp
+++ Entity/item.cpp
@@ -1,13 +1,5 @@
 #include "item.h"
 
-item::item(std::string name, int x, int y, bool isConsumedOnUse)
-{
-    this->name = name;
-    this->x = x;
-    this->y = y;
-    this->isConsumedOnUse = isConsumedOnUse;
-    this->symbol = '?';   // Default symbol for items
-}
 std::string item::getName() const
 {
     return name;
