dea228c9a7529d88eaf48dcd2c8e9c2b4da6713d
ShiroXqC <hiamgosunabu@gmail.com>
2025-05-07 21:53:38 +0700

    backup complete

---
+++ Trash/T/Entity.h
@@ -0,0 +1,55 @@
+#pragma once
+#include<iostream>
+#include<string>
+#include<vector>
+
+class Entity
+{
+    protected:
+    int hp; 
+    int attackpower;
+    int exp;
+    int x, y;
+    const bool isAlive = true;
+    
+
+    public:
+    //Constructor overload
+     Entity(int hp, int attackpower, int exp, int startX, int startY)
+    
+    {
+        this->hp=hp;
+        this->attackpower=attackpower;
+        this->exp=exp;
+        this->x=startX;
+        this->y=startY;
+    }
+    
+    //Default Constructor 
+     Entity()
+    {
+        this->hp=0;
+        this->attackpower=0;
+        this->exp=0;
+        this->x=0;
+        this->y=0;
+    }
+
+    //Getters
+    int getHp() const;
+    int getAttackpower() const;
+    int getExp() const;
+    int getX() const;
+    int getY() const;
+    bool getIsAlive() const;
+    //Setters
+    void setHp(int hp);
+    void setAttackpower(int attackpower);
+    void setExp(int exp);
+    void setY(int y);
+    void setX(int x); // Fixed: Changed parameter name from y to x
+    void setPosition(int newX, int newY);
+    void takeDamage(int amount);
+    //Virtual function
+    virtual void attack(Entity &Target);
+};
---
+++ Trash/T/Inventory.h
@@ -0,0 +1,30 @@
+#pragma once 
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+#include <memory> // For std::unique_ptr dynamic memory management 
+#include "Item.h" // Include the Item class header
+
+class Player; // Forward declaration of Player class
+ 
+class Item; // Forward declaration of Item class
+
+class Inventory {
+    protected:
+    std::vector<std::unique_ptr<Item>> items; // Vector to store item names
+    int capacity; // Maximum capacity of the inventory
+    public:
+    Inventory() : capacity(10) {} // Default constructor 
+    Inventory(int capacity);
+
+    bool addItem(std::unique_ptr<Item> item); // Add an item to the inventory
+    void listItems() const; // List all items in the inventory
+    void useItem(int index, Player& player); // Use an item from the inventory
+
+
+
+    // Getters
+    int getCapacity() const;
+    int getItemCount() const {return static_cast<int>(items.size()); }
+};
